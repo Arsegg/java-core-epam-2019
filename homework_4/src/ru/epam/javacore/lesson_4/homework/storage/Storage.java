0254e83c50b0fbef910e9f455f6ccb9da838350c
Arsegg <arsegg@mail.ru>
2019-12-11 14:45:03 +0300

    issue #4

--- homework_4/src/ru/epam/javacore/lesson_4/homework/storage/Storage.java
+++ homework_4/src/ru/epam/javacore/lesson_4/homework/storage/Storage.java
@@ -1,18 +1,25 @@
 package ru.epam.javacore.lesson_4.homework.storage;
 
 import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 public class Storage {
     private Object[] storage = {};
 
-    // Ultra-slow method - O(n ^ 2) time complexity and exact memory consumption (no pre-allocated items)
+//    // Ultra-slow method - O(n ^ 2) time complexity and exact memory consumption (no pre-allocated items)
+//    public void add(Object object) {
+//        var storageStream = Arrays.stream(storage);
+//        var objectStream = Stream.of(object);
+//        storage = Stream.concat(storageStream, objectStream)
+//                .toArray();
+//    }
+
+    // Another ultra-slow method - O(n ^ 2) time complexity and exact memory consumption (no pre-allocated items)
     public void add(Object object) {
-        var storageStream = Arrays.stream(storage);
-        var objectStream = Stream.of(object);
-        storage = Stream.concat(storageStream, objectStream)
-                .toArray();
+        var newStorage = new Object[storage.length + 1];
+        System.arraycopy(storage, 0, newStorage, 0, storage.length);
+        newStorage[newStorage.length - 1] = object;
+
+        storage = newStorage;
     }
 
     public void printAll() {
